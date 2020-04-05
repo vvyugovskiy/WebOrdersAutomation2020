42de67f4226f63351ec364f2f6b0c665d62890a0
vvyugovskiy <resifar@gmail.com>
2020-04-05 13:53:50 -0400

    first commit

---
+++ src/test/java/com/weborders/pages/LoginPage.java
@@ -0,0 +1,35 @@
+package com.weborders.pages;
+
+import com.weborders.utilities.ConfigurationReader;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends AbstractBasePage {
+
+    @FindBy(id = "ctl00_MainContent_username")
+    private WebElement userName;
+
+    @FindBy(id = "ctl00_MainContent_password")
+    private WebElement password;
+
+    public void login() {
+        String userNameValue = ConfigurationReader.getProperty("username");
+        String passwordValue = ConfigurationReader.getProperty("password");
+        userName.sendKeys(userNameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+
+    }
+
+    /**
+     * Method to login, version #1
+     * Login as a specific user
+     *
+     * @param usernameValue
+     * @param passwordValue
+     */
+    public void login(String usernameValue, String passwordValue) {
+        userName.sendKeys(usernameValue);
+        password.sendKeys(passwordValue, Keys.ENTER);
+    }
+}
